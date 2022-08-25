817875e8a163414c5f97cc42bb93bd2e82fa3d9b
unknown <yashas hr>
2022-08-25 11:43:31 +0530

    final review frontend

---
+++ src/utils/options.js
@@ -0,0 +1,21 @@
+export const typeOptions = [
+  { title: "Film", value: "Film" },
+  { title: "Short Film", value: "Short Film" },
+  { title: "Documentary", value: "Documentary" },
+  { title: "TV Series", value: "TV Series" },
+  { title: "Web Series", value: "Web Series" },
+];
+
+export const statusOptions = [
+  { title: "Public", value: "public" },
+  { title: "Private", value: "private" },
+];
+
+export const languageOptions = [
+  { title: "English", value: "English" },
+  { title: "Hindi", value: "Hindi" },
+  { title: "Tamil", value: "Tamil" },
+  { title: "Telugu", value: "Telugu" },
+  { title: "Kannada", value: "Kannada" },
+  { title: "Malayalam", value: "Malayalam" },
+];
